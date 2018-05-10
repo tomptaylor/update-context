48910e59d60ec0e1cf95b8ad6f7ff3dae49c122e
Tom Taylor <tomptaylor@gmail.com>
2018-05-10 16:32:00 -0600

    Create update-context.js

---
+++ update-context.js
@@ -0,0 +1,22 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+
+module.exports = function(context, cb) {
+  const MONGO_URL = context.secrets.DB_KEY;
+  var mytable = context.body.table;
+  delete context.body.table;
+  console.log(mytable);
+  const model = context.body;
+
+  MongoClient.connect(MONGO_URL, (err, result) => {
+    if (err) console.log('err');
+      let db = result.db('testtom');
+           var collection = db.collection(`'${context.body.table}'`);
+ // console.log(collection);
+
+    db.collection(`${mytable}`).insertOne(model, (err, result) => {
+      if (err)  cb(err,null);
+      cb(null, result);
+    });
+  });
+};
